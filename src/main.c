08a804ca0a11193bc0cab9f7c103038ee97c51d1
Jarsikan <sergio.marin.alvarez@gmail.com>
2025-02-19 11:37:31 +0100

    añadido mapa de prueba he inico de parseo

--- src/main.c
+++ src/main.c
@@ -6,45 +6,63 @@
 /*   By: smarin-a <smarin-a@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 14:42:34 by jariskan          #+#    #+#             */
-/*   Updated: 2025/02/18 15:21:21 by smarin-a         ###   ########.fr       */
+/*   Updated: 2025/02/19 11:27:43 by smarin-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+// typedef struct	s_data {
+// 	void	*img;
+// 	char	*addr;
+// 	int		bits_per_pixel;
+// 	int		line_length;
+// 	int		endian;
+// }				t_data;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+// void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+// {
+// 	char	*dst;
+
+// 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+// 	*(unsigned int*)dst = color;
+// }
+
+// int	main(void)
+// {
+// 	void	*mlx;
+// 	void	*mlx_win;
+// 	t_data	img;
+
+// 	mlx = mlx_init();
+//     if(!mlx)
+//     {
+//         printf("ERROR\n");
+//         return (0);
+//     }
+// 	mlx_win = mlx_new_window(mlx, 400, 400, "Hello world!");
+// 	img.img = mlx_new_image(mlx, 400, 400);
+// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+// 								&img.endian);
+// 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+// 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+// 	mlx_loop(mlx);
+// }
+
+int ft_check_map_extension(char *map_file)
 {
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	
 }
 
-int	main(void)
+int main(int argc, char **argv)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-
-	mlx = mlx_init();
-    if(!mlx)
-    {
-        printf("ERROR\n");
-        return (0);
-    }
-	mlx_win = mlx_new_window(mlx, 400, 400, "Hello world!");
-	img.img = mlx_new_image(mlx, 400, 400);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	if (argc != 2)
+		printf("Invalid number of arguments\n");
+	// Verificar extension del mapa.
+	if (ft_check_map_extension(argv[1]))
+	{
+		printf("Error\nInvalid map extension\n");
+		return (1);
+	}
+	
 }
