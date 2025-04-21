b2fd477a50be9de8fc48dfe0c49a9f1dc1724c9f
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-04-21 23:56:48 +0200

    fix: now you can hold movement buttons

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
+/*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 14:42:34 by jariskan          #+#    #+#             */
-/*   Updated: 2025/04/21 18:12:59 by jdelorme         ###   ########.fr       */
+/*   Updated: 2025/04/21 23:54:52 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,8 @@ int	main(int argc, char **argv)
 	// Funcion para imprimir texturas y actualizarlas --> so_long ft_print_sprites(&window, game.map);
 	mlx_loop_hook(pgm.window.mlx, ft_render_frame, &pgm);
 	mlx_hook(pgm.window.win, 17, 0, ft_close_window, &pgm);
-	mlx_key_hook(pgm.window.win, ft_handle_keys, &pgm);
+	mlx_hook(pgm.window.win, 2, 1L << 0, ft_handle_keys, &pgm); // key press
+
 	
 	
 	mlx_loop(pgm.window.mlx);
