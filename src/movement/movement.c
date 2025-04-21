edd0586016af825cdb66e67c1daa3d177fe3a58c
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-04-21 23:20:49 +0200

    add: keys ASD to player movement

--- src/movement/movement.c
+++ src/movement/movement.c
@@ -6,7 +6,7 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 17:26:58 by jdelorme          #+#    #+#             */
-/*   Updated: 2025/04/21 22:16:37 by volmer           ###   ########.fr       */
+/*   Updated: 2025/04/21 23:20:05 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,33 @@ int	ft_handle_keys(int keycode, t_pgm *pgm)
 	}
 	if (keycode == KEY_S)
 	{
-		//logica atrás satanás
+		if (pgm->map.map[(int)(pgm->game.pos_y)]
+                [(int)(pgm->game.pos_x - pgm->game.dir_x * SPEED)] != '1')
+			pgm->game.pos_x -= pgm->game.dir_x * SPEED;
+
+		if (pgm->map.map[(int)(pgm->game.pos_y - pgm->game.dir_y * SPEED)]
+                [(int)(pgm->game.pos_x)] != '1')
+			pgm->game.pos_y -= pgm->game.dir_y * SPEED;
 	}
-	return 0;
+	if (keycode == KEY_A)
+	{
+		if (pgm->map.map[(int)(pgm->game.pos_y)]
+	               [(int)(pgm->game.pos_x - pgm->game.plane_x * SPEED)] != '1')
+			pgm->game.pos_x -= pgm->game.plane_x * SPEED;
+		
+		if (pgm->map.map[(int)(pgm->game.pos_y - pgm->game.plane_y * SPEED)]
+	                [(int)(pgm->game.pos_x)] != '1')
+			pgm->game.pos_y -= pgm->game.plane_y * SPEED;
+	}
+	if (keycode == KEY_D)
+	{
+		if (pgm->map.map[(int)(pgm->game.pos_y)]
+	                [(int)(pgm->game.pos_x + pgm->game.plane_x * SPEED)] != '1')
+			pgm->game.pos_x += pgm->game.plane_x * SPEED;
+
+		if (pgm->map.map[(int)(pgm->game.pos_y + pgm->game.plane_y * SPEED)]
+	                [(int)(pgm->game.pos_x)] != '1')
+			pgm->game.pos_y += pgm->game.plane_y * SPEED;
+	}
+	return (0);
 }
