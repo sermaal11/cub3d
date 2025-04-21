1570efd06d08bd9b7f51f86cbfc9cc4d2bf8f86f
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-04-21 23:51:05 +0200

    fix: rotation movement and normi

--- src/movement/movement.c
+++ src/movement/movement.c
@@ -6,7 +6,7 @@
 /*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 17:26:58 by jdelorme          #+#    #+#             */
-/*   Updated: 2025/04/21 23:20:05 by volmer           ###   ########.fr       */
+/*   Updated: 2025/04/21 23:46:37 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,43 +17,12 @@ int	ft_handle_keys(int keycode, t_pgm *pgm)
 	if (keycode == KEY_ESC)
 		ft_close_window(pgm);
 	if (keycode == KEY_W)
-	{
-		if (pgm->map.map[(int)(pgm->game.pos_y)]
-	                [(int)(pgm->game.pos_x + pgm->game.dir_x * SPEED)] != '1')
-			pgm->game.pos_x += pgm->game.dir_x * SPEED;
-		if (pgm->map.map[(int)(pgm->game.pos_y + pgm->game.dir_y * SPEED)]
-	            [(int)(pgm->game.pos_x)] != '1')
-		pgm->game.pos_y += pgm->game.dir_y * SPEED;	
-	}
-	if (keycode == KEY_S)
-	{
-		if (pgm->map.map[(int)(pgm->game.pos_y)]
-                [(int)(pgm->game.pos_x - pgm->game.dir_x * SPEED)] != '1')
-			pgm->game.pos_x -= pgm->game.dir_x * SPEED;
-
-		if (pgm->map.map[(int)(pgm->game.pos_y - pgm->game.dir_y * SPEED)]
-                [(int)(pgm->game.pos_x)] != '1')
-			pgm->game.pos_y -= pgm->game.dir_y * SPEED;
-	}
-	if (keycode == KEY_A)
-	{
-		if (pgm->map.map[(int)(pgm->game.pos_y)]
-	               [(int)(pgm->game.pos_x - pgm->game.plane_x * SPEED)] != '1')
-			pgm->game.pos_x -= pgm->game.plane_x * SPEED;
-		
-		if (pgm->map.map[(int)(pgm->game.pos_y - pgm->game.plane_y * SPEED)]
-	                [(int)(pgm->game.pos_x)] != '1')
-			pgm->game.pos_y -= pgm->game.plane_y * SPEED;
-	}
-	if (keycode == KEY_D)
-	{
-		if (pgm->map.map[(int)(pgm->game.pos_y)]
-	                [(int)(pgm->game.pos_x + pgm->game.plane_x * SPEED)] != '1')
-			pgm->game.pos_x += pgm->game.plane_x * SPEED;
-
-		if (pgm->map.map[(int)(pgm->game.pos_y + pgm->game.plane_y * SPEED)]
-	                [(int)(pgm->game.pos_x)] != '1')
-			pgm->game.pos_y += pgm->game.plane_y * SPEED;
-	}
+		move_forward(pgm);
+	else if (keycode == KEY_S)
+		move_backward(pgm);
+	else if (keycode == KEY_A)
+		rotate_left(pgm);
+	else if (keycode == KEY_D)
+		rotate_right(pgm);
 	return (0);
 }
