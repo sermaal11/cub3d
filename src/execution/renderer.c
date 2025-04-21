49995a61a0ddf5da1c90a455383a4c3e1f5e9e06
Juan Bautista Delorme Pinedo <jdelorme@c2r6s4.42madrid.com>
2025-04-21 14:31:18 +0200

    fix: too long funciton

--- src/execution/renderer.c
+++ src/execution/renderer.c
@@ -6,52 +6,56 @@
 /*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 12:19:12 by jdelorme          #+#    #+#             */
-/*   Updated: 2025/04/21 14:19:18 by jdelorme         ###   ########.fr       */
+/*   Updated: 2025/04/21 14:30:37 by jdelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-int	ft_render_frame(t_pgm *pgm)
+static void ft_init_ray_direction(t_ray *ray, t_pgm *pgm)
 {
-	t_ray ray;
-	
 	/* Si usamos la posiciones de valor entero directamente, estariamos 
 	indicando las esquina superior izquierda del bloque al jugador, le sumamos
 	la mitad de un entero dentro del double para poder indicar que nuestro 
 	jugador se encuentra en medio medio del bloque y calcular correcamente 
 	el trazado de rayos y evitar posibles bugs en la esquinas.*/
-	ray.pos_x = pgm->game.y_plyr + 0.5;
-	ray.pos_y = pgm->game.x_plyr + 0.5;
+	ray->pos_x = pgm->game.y_plyr + 0.5;
+	ray->pos_y = pgm->game.x_plyr + 0.5;
 	
 	//convertir en valores numericos la orientacion del jugador en 2D
 	if (pgm->game.dir == 'N')
 	{
-		ray.dir_x = 0;
-		ray.dir_y = -1;
+		ray->dir_x = 0;
+		ray->dir_y = -1;
 	}
 	if (pgm->game.dir == 'S')
 	{
-		ray.dir_x = 0;
-		ray.dir_y = 1;
+		ray->dir_x = 0;
+		ray->dir_y = 1;
 	}
 	if (pgm->game.dir == 'E')
 	{
-		ray.dir_x = 1;
-		ray.dir_y = 0;
+		ray->dir_x = 1;
+		ray->dir_y = 0;
 	}
 	if (pgm->game.dir == 'W')
 	{	
-		ray.dir_x = -1;
-		ray.dir_y = 0;
+		ray->dir_x = -1;
+		ray->dir_y = 0;
 	}
 	
 	/* Esto va ajustar el punto de vista mediante un plano perpendicular
 	que nos indicará la vision del jugador (FOV ajustable en define) */
-	ray.plane_x = FOV * -ray.dir_y;
-	ray.plane_y = FOV * ray.dir_x;
+	ray->plane_x = FOV * -ray->dir_y;
+	ray->plane_y = FOV * ray->dir_x;
 
+}
+
+int	ft_render_frame(t_pgm *pgm)
+{
+	t_ray ray;
 	
+	ft_init_ray_direction(&ray, pgm);
 	ray.x = 0; // Es la columna vertical que estamos pintando
 	while (ray.x < WIDTH) //De izq a der de la pantalla calcularemos rayos
 	{
