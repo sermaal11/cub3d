a3b34d6eeea979d63284eadc8cb87c0b02936d64
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2025-04-30 00:51:32 +0200

    fix: norminette

--- src/utils/rendering_utils.c
+++ src/utils/rendering_utils.c
@@ -3,20 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   rendering_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
+/*   By: volmer <volmer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 17:50:15 by jariskan          #+#    #+#             */
-/*   Updated: 2025/04/22 13:33:18 by jdelorme         ###   ########.fr       */
+/*   Updated: 2025/04/30 00:51:08 by volmer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	ft_direciton_west(t_game *game)
+{
+	game->dir_x = -1;
+	game->dir_y = 0;
+	game->plane_x = 0;
+	game->plane_y = 0.66;
+}
+
 void	ft_init_struct_rendering(t_pgm *pgm)
 {
 	pgm->window.mlx = NULL;
-	pgm->window.win = NULL;	
+	pgm->window.win = NULL;
 }
+
 void	ft_init_player_orientation(t_game *game)
 {
 	if (game->dir == 'N')
@@ -42,13 +51,11 @@ void	ft_init_player_orientation(t_game *game)
 	}
 	if (game->dir == 'W')
 	{
-		game->dir_x = -1;
-		game->dir_y = 0;
-		game->plane_x = 0;
-		game->plane_y = 0.66;
+		ft_direciton_west(game);
 	}
 }
-int ft_rgb_to_int(t_vec3 color)
+
+int	ft_rgb_to_int(t_vec3 color)
 {
 	return ((int)(color.r) << 16 | (int)(color.g) << 8 | (int)(color.b));
 }
